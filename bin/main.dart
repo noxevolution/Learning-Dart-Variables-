b07c4923ace4f4ff5d6847c2c2c8d854b8c8fc47
noxevolution <proloyd4@gmail.com>
2018-03-04 04:40:54 +0530

    More on Variable Arithmatic, Conditions, Loops...

--- bin/main.dart
+++ bin/main.dart
@@ -21,4 +21,38 @@ main(List<String> arguments) {
   print (title);
 
   print(title.substring(0,4));
+
+  title = title.replaceAll('ll', 'zz');
+  print (title);
+
+  String names = "Pro,Boba,Baka,Pop";
+  assert(names.contains("Baka"));
+  
+  List<String> list = names.split(",");
+  print(list);
+
+  print(list.elementAt(2));
+  
+  list.forEach((f){
+    print (f);
+  });
+
+  Map<String, int> ages = {
+    'Pro' : 4,
+    'Baka' : 3,
+    'Googly' : 1,
+  };
+
+  print (ages.keys);
+  print (ages.values);
+
+  ages['Habla'] = 7;
+  ages['Ghatok'] = 8;
+
+  print (ages[ages.keys.elementAt(2)]);
+
+  ages.forEach((k, v){
+    print ("$k is standing in $v position");
+  });
+
 }
